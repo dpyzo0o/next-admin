e4c6e7effe3f24c6ba5746383f91d23d54d1acf1
dpyzo0o <dpyzo0o@outlook.com>
2020-09-24 19:15:53 +0800

    chore: less to js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const bundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const lessToJs = require('less-vars-to-js');
 const antdLess = require('./src/next-plugins/antd-less');
 
-const modifyVars = {
-  'primary-color': '#e93d34',
-};
+const modifyVars = lessToJs(
+  fs.readFileSync(path.resolve(__dirname, './src/styles/antd-custom.less'), 'utf8')
+);
 
 module.exports = withPlugins([
   [bundleAnalyzer],
