ea118fba4ba44c5de53e06d4b8b580ee604b6617
dpyzo0o <dpyzo0o@outlook.com>
2020-12-09 18:24:45 +0800

    chore: test

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -2,6 +2,5 @@ import { withSession } from 'src/utils/session';
 
 export default withSession(async (req, res) => {
   req.session.destroy();
-  res.setHeader('cache-control', 'public, s-maxage=0, must-revalidate');
   res.send('logged out');
 });
