867f8ebf6ece97698524dff429bf4b6ae9e0ef37
dpyzo0o <dpyzo0o@outlook.com>
2020-12-09 23:08:32 +0800

    fix: api route caching

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,6 +1,8 @@
 import { withSession } from 'src/utils/session';
 
-export default withSession(async (req, res) => {
+export default withSession((req, res) => {
   req.session.destroy();
+  // https://github.com/vvo/next-iron-session/issues/274
+  res.setHeader('cache-control', 'no-store, max-age=0');
   res.send('logged out');
 });
